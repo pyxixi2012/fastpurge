f5f9b534930cc4cea8a35a3e2e850febf2d8fc4f
Mikael Voss <mikael@illdefined.org>
2012-02-18 16:16:55 +0000

    BaseAdapter: Provide correct constructor

--- src/base_adapter.cpp
+++ src/base_adapter.cpp
@@ -1,8 +1,5 @@
-class BaseAdapter {
-
-  BaseAdapter(struct ev_loop* ev_, const std::vector<std::string>& patterns_, adapter_config* config_) : 
-    ev(ev_), patterns(patterns_), config(config_) {
-    printf('initialized');
-  }
-
+BaseAdapter::BaseAdapter(struct ev_loop* ev_,
+	const std::vector<std::string>& patterns_,
+	adapter_config* config_)
+		: ev(ev_), patterns(patterns_), config(config_) {
 }
